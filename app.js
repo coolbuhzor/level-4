47c86583e6fe0c6d988deecc045b7a61c435daa8
buhzor <coolbuhzor@gmail.com>
2020-11-03 18:37:25 +0100

    added media querries

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let blue = document.querySelector('.blue').addEventListener('click', function() {
+document.querySelector('.blue').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background= '#0392bf'
     document.querySelector('.whole-page').style.color = 'white'
     document.getElementById('final-btn').style.background = '#ed48ba'
     document.getElementById('link').style.color = 'white'
 })
 
-let pink = document.querySelector('.pink').addEventListener('click', function() {
+document.querySelector('.pink').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background= '#ed48ba'
     document.querySelector('.whole-page').style.color = 'white'
     document.getElementById('final-btn').style.background = '#000'
@@ -13,20 +13,21 @@ let pink = document.querySelector('.pink').addEventListener('click', function() 
 
 })
 
-let yellow = document.querySelector('.yellow').addEventListener('click', function() {
+document.querySelector('.yellow').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background = '#FBEC06'
     document.querySelector('.whole-page').style.color = '#000'
     document.getElementById('link').style.color = '#000'
     document.getElementById('final-btn').style.background = '#ed48ba'
-  
+
     
 })
 
-let grey = document.querySelector('.white').addEventListener('click', function() {
+document.querySelector('.white').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background = '#B3D4E0'
     document.querySelector('.whole-page').style.color = '#000'
     document.getElementById('final-btn').style.background = '#ed48ba'
     document.querySelector('#link').style.color = '#000'
+
     
     
 })
