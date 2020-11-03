1f514231beba8c923230b8e2739f8dec19bdaa08
buhzor <coolbuhzor@gmail.com>
2020-11-03 19:02:11 +0100

    adjusted the page

--- app.js
+++ app.js
@@ -2,22 +2,43 @@ document.querySelector('.blue').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background= '#0392bf'
     document.querySelector('.whole-page').style.color = 'white'
     document.getElementById('final-btn').style.background = '#ed48ba'
-    document.getElementById('link').style.color = 'white'
+    var els = document.getElementsByClassName('fff');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#000';
+    }
+    var els = document.getElementsByClassName('fa-star');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#fbec06';
+    }
 })
 
 document.querySelector('.pink').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background= '#ed48ba'
     document.querySelector('.whole-page').style.color = 'white'
     document.getElementById('final-btn').style.background = '#000'
-    document.querySelector('#link').style.color = 'white'
+    var els = document.getElementsByClassName('link');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#fff';
+    }
+    var els = document.getElementsByClassName('fa-star');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#fbec06';
+    }
 
 })
 
 document.querySelector('.yellow').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background = '#FBEC06'
     document.querySelector('.whole-page').style.color = '#000'
-    document.getElementById('link').style.color = '#000'
+    var els = document.getElementsByClassName('link');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#000';
+    }
     document.getElementById('final-btn').style.background = '#ed48ba'
+    var els = document.getElementsByClassName('fa-star');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#ed48ba';
+    }
 
     
 })
@@ -26,7 +47,14 @@ document.querySelector('.white').addEventListener('click', function() {
     document.querySelector('.whole-page').style.background = '#B3D4E0'
     document.querySelector('.whole-page').style.color = '#000'
     document.getElementById('final-btn').style.background = '#ed48ba'
-    document.querySelector('#link').style.color = '#000'
+    var els = document.getElementsByClassName('link');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#000';
+    }
+    var els = document.getElementsByClassName('fa-star');
+    for (var i = 0; i < els.length; i++) {
+      els[i].style.color = '#fbec06';
+    }
 
     
     
